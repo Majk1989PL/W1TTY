92b4893971924cc3144ab1ab0977d490ed81f40f
Devskiller <owl@devskiller.com>
2023-11-02 12:48:32 +0000

    Candidate repo initialization

---
+++ src/main/java/com/devskiller/library/service/ResourceLoader.java
@@ -0,0 +1,11 @@
+package com.devskiller.library.service;
+
+import java.util.Optional;
+
+public class ResourceLoader {
+
+    public static Optional<String> getProperty(String name) {
+        throw new UnsupportedOperationException("getProperty");
+    }
+
+}
